81359ecc957d5261a8f8d73745b6bdf1ee74c0ba
mvishok <vishokmanikantan@gmail.com>
2026-05-02 11:13:41 +0530

    feat: initialize project with Express, TypeScript, and logging middleware integration

---
+++ logging_middleware/logger.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { env } from "node:process";
+
+const LOG_API = "http://20.207.122.201/evaluation-service/logs";
+
+export const Log = async (
+    stack: "backend",
+    level: "debug" | "info" | "warn" | "error" | "fatal",
+    pkg:
+        | "cache"
+        | "controller"
+        | "cron_job"
+        | "db"
+        | "domain"
+        | "handler"
+        | "repository"
+        | "route"
+        | "service",
+    message: string,
+    token: string
+) => {
+    try {
+        const response = await axios.post(
+            LOG_API,
+            {
+                stack,
+                level,
+                package: pkg,
+                message
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+        console.log("received following response", response.data);
+        return response.data;
+    } catch (err: any) {
+        console.error("Log failed:", err.response?.data || err.message);
+        console.log(`[FALLBACK LOG] [${level.toUpperCase()}] ${stack} (${pkg}): ${message}`);
+        return null;
+    }
+};
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Log } from "../logging_middleware/logger.js";
+
+dotenv.config();
+console.log("booted");
+const app = express();
+app.use(express.json());
+
+const TOKEN = process.env.TOKEN!;
+
+app.get("/", async (req, res) => {
+    await Log("backend", "info", "route", "Root route hit", TOKEN);
+    res.send("Working");
+});
+
+app.get("/error", async (req, res) => {
+    console.log("route hit");
+    const re = await Log("backend", "error", "handler", "Test error log", TOKEN);
+    console.log("received response", re);
+    res.status(500).send("Error route");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on 3000");
+});
